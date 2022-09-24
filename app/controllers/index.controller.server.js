b46f187dfb323b8a74fd70059bd649a318e547ae
Sheila Donnelly <sheila.donnelly@outlook.com>
2022-09-23 17:40:26 -0700

    next save point

--- app/controllers/index.controller.server.js
+++ app/controllers/index.controller.server.js
@@ -1,6 +1,5 @@
 //Index Controller - single exportable function
 export function message(req, res, next){
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World')
+    res.render('index', { title: 'Home' });
 };
 
