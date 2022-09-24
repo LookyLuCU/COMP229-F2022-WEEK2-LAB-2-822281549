67c71b0cd6c8749f984cd7828b8580d0ea9d0c1b
Sheila Donnelly <sheila.donnelly@outlook.com>
2022-09-23 18:10:43 -0700

    and more

--- app/routes/index.server.route.js
+++ app/routes/index.server.route.js
@@ -1,12 +1,20 @@
-//Index routes
+// Index Routes
 import { Router } from 'express';
-import helloWorld from '../controllers/index.controller.server.js' //make sure to add extension (.js)
+import { displayHomePage, 
+    displayAboutPage, 
+    displayProjectsPage, 
+    displayServicesPage, 
+    displayContactPage } from '../controllers/index.controller.server.js'
 
-//instatntiating router
+// Intanciating the router
 const router = Router();
 
-//add middleware to connect application
-router.get('/', helloworld);
-router.get('/hello', helloworld);
+// add middleware to connect application
+router.get('/',displayHomePage);
+router.get('/home',displayHomePage);
+router.get('/about', displayAboutPage );
+router.get('/projects', displayProjectsPage );
+router.get('/services', displayServicesPage );
+router.get('/contact', displayContactPage );
 
 export default router;
